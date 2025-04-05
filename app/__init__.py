67ab7deb370723ec2ac31ba5078fd0de8055d938
Daniel <danielrasgadoc@outlook.es>
2025-04-05 12:00:27 -0600

    update

--- app/__init__.py
+++ app/__init__.py
@@ -6,11 +6,9 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(settings)
 
-    # Inicializa TinyDB y lo guarda en la configuración de la app
     db = TinyDB(app.config['TINIDB_PATH'])
     app.config['DB'] = db
 
-    # Importa y registra las rutas mediante Blueprint
     from app import routes
     app.register_blueprint(routes.bp)
 
